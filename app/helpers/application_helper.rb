3937bb3cb5030c6136e517e991b46f595d7a9c11
hoverlover <hoverlover@gmail.com>
2011-02-21 13:17:10 -0600

    Added some links to github objects.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def github_base_url
+    'http://github.com'
+  end
 end
--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -2,4 +2,8 @@ module MembersHelper
   def repo_name_from_commit_url(url)
     url.split('/').delete_if(&:empty?).second
   end
+
+  def short_sha(commit_id)
+    commit_id.first(7)
+  end
 end
